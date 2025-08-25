8e4b229ed41fb9055cb8d7cb14fe20bab490d906
Leonardo Bourscheid <leonardojohannbourscheid@gmail.com>
2025-08-24 23:53:38 -0300

    feat: created test profile using H2 database and JPA

--- src/main/java/com/learningspring/course/entities/User.java
+++ src/main/java/com/learningspring/course/entities/User.java
@@ -1,16 +1,26 @@
 package com.learningspring.course.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "users")
 public class User
-    implements
-        Serializable
+        implements
+            Serializable
 {
     @Serial
     private static final long serialVersionUID = 1L;
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
     String name;
     String email;
